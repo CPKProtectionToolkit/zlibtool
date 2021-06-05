4933de6d4ba41f5171abe9b634fe39ff52f9c8dd
Ionut Trandafir <81416749+CPKProtectionToolkit@users.noreply.github.com>
2021-06-05 03:09:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -151,29 +151,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-QT -= gui
-
-CONFIG += c++11 console
-CONFIG -= app_bundle
-
-TARGET = zlibtool
-TEMPLATE = app
-
-VERSION = 1.0.0
-QMAKE_TARGET_COMPANY = "IT World\\256 Software Solutions"
-QMAKE_TARGET_PRODUCT = "ZlibTool"
-QMAKE_TARGET_DESCRIPTION = "ZlibTool"
-QMAKE_TARGET_COPYRIGHT = "\\251 IT World\\256 Software Solutions"
-
-# You can make your code fail to compile if it uses deprecated APIs.
-# In order to do so, uncomment the following line.
-#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
-
-SOURCES += \
-        main.cpp
-
-# Default rules for deployment.
-qnx: target.path = /tmp/$${TARGET}/bin
-else: unix:!android: target.path = /opt/$${TARGET}/bin
-!isEmpty(target.path): INSTALLS += target
